125f445563f0ba58282af051ac42f9d9c30e2778
Saadullah <saadulla@my.yorku.ca>
2019-11-05 22:34:48 +0500

    Browsing to homepage

--- scraper.py
+++ scraper.py
@@ -1,1 +1,6 @@
-import selenium
+from selenium import webdriver
+
+# create a new Firefox session
+
+driver = webdriver.Chrome()
+driver.get("https://www.bell.ca/Mobility/Smartphones_and_mobile_internet_devices")
